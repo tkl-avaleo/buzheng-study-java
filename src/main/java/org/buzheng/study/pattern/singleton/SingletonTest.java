464d1afe38f64949e891accce5ec60470e9297f7
buzheng <zhjiun@gmail.com>
2012-12-13 17:28:15 +0800

    singleton

--- src/main/java/org/buzheng/study/pattern/singleton/SingletonTest.java
+++ src/main/java/org/buzheng/study/pattern/singleton/SingletonTest.java
@@ -3,7 +3,7 @@ package org.buzheng.study.pattern.singleton;
 public class SingletonTest {
 	public static void main(String[] args) {
 		System.out.println("-------------------------------");
-//		Singleton.getInstance();
-		Singleton.anotherStaticMethod();
+		Singleton.getInstance();
+//		Singleton.anotherStaticMethod();
 	}
 }
