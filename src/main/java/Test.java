9c9eb39bd276be926379b12019788ff7038183ba
buzheng <zhjiun@gmail.com>
2012-12-14 18:20:37 +0800

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 public class Test {	
 	public static void main(String[] args) throws Exception {
 		System.out.println("just a string, here.");
+		
+		java.util.Observable observable;
+		java.util.Observer observer;
+		
+		Class.forName("");
 	}	
 }
