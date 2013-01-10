058deb8aee6c3ac43c4ce6e778448024da453302
buzheng <zhjiun@gmail.com>
2013-01-10 11:53:49 +0800

    quartz modification

--- src/main/java/org/buzheng/study/quartz/MainCluster.java
+++ src/main/java/org/buzheng/study/quartz/MainCluster.java
@@ -4,15 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * quartz 普通调用
+ * Quartz 集群支持测试
  *
  */
-public class Main {
+public class MainCluster {
 
-	@SuppressWarnings({ "unused", "resource" })
+	@SuppressWarnings({ "resource", "unused" })
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] {"classpath:org/buzheng/study/quartz/applicationContext-schedule.xml"});
+				new String[] {"classpath:org/buzheng/study/quartz/applicationContext-cluster.xml"});
 		
 	}
 
